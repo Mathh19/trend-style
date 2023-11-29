6c8e95b3c7814fc90fdb7a0449b73bb9705b6cbb
Matheus <mf635097@gmail.com>
2023-11-29 15:28:53 -0300

    feat: add component ContainerProducts

---
+++ src/components/ContainerProducts.tsx
@@ -0,0 +1,29 @@
+import { Product, ProductProps } from './Product';
+
+type ContainerProductsProps = {
+  title: string;
+  products: ProductProps[];
+};
+
+export const ContainerProducts = ({
+  title,
+  products
+}: ContainerProductsProps) => {
+  return (
+    <section className="text-center space-y-7 py-11">
+      <h2 className="text-4xl">{title}</h2>
+      <div className="flex justify-around w-full flex-wrap gap-16">
+        {products.map((product, index) => (
+          <Product
+            key={index}
+            name={product.name}
+            price={product.price}
+            rate={product.rate}
+            img={product.img}
+            discount={product.discount}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
