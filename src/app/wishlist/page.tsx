e87ec4871cba31b910852915f60929a5138c8d8c
Matheus <mf635097@gmail.com>
2024-01-26 14:35:57 -0300

    feat: add WishList page

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useContext, useEffect, useRef, useState } from 'react';
+import { HiOutlineInbox } from 'react-icons/hi2';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
+
+import { ContainerProducts } from '@components/ContainerProducts';
+import { ProductProps } from '@components/Product';
+import { WishlistContext } from '@contexts/WishlistContext/WishlistContext';
+
+export default function Wishlist() {
+  const { products, clearWishList } = useContext(WishlistContext);
+  const [limitProduct, setLimitProduct] = useState(6);
+  const [maxProducts, setMaxProducts] = useState<ProductProps[]>([]);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleIncreaseLimit = () => {
+    setLimitProduct((prevLimitProduct) => prevLimitProduct + 3);
+    setTimeout(() => {
+      buttonRef.current?.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }, 180);
+  };
+
+  useEffect(() => {
+    setMaxProducts(products.slice(0, limitProduct));
+  }, [limitProduct, products]);
+
+  return (
+    <div className="flex flex-col items-center min-h-screen gap-4 px-14">
+      <h2 className="text-center uppercase font-bold text-3xl">My wishlist</h2>
+      {products.length === 0 ? (
+        <div className="flex flex-col items-center">
+          <p className="text-2xl">You have no products in your wish list</p>
+          <HiOutlineInbox size={140} className="mt-20" />
+        </div>
+      ) : (
+        <>
+          <div className="w-full flex max-w-[1096px] justify-end">
+            <button
+              onClick={clearWishList}
+              className="bg-black text-white text-sm drop-shadow-md py-1.5 px-2 flex gap-1 items-center"
+            >
+              clear wishlist
+              <IoIosCloseCircleOutline />
+            </button>
+          </div>
+          <ContainerProducts products={maxProducts} />
+          {products.length !== maxProducts.length && (
+            <button
+              ref={buttonRef}
+              onClick={handleIncreaseLimit}
+              className="bg-black text-white rounded-md py-1 px-1.5"
+            >
+              see more
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,9 @@
 import { Keania_One } from 'next/font/google';
 import Image from 'next/image';
-import { FiShoppingCart, FiHeart } from 'react-icons/fi';
+import Link from 'next/link';
+import { FiShoppingCart, FiHeart, FiUser } from 'react-icons/fi';
 
 import { MenuLinks } from '@components/MenuLinks/MenuLinks';
-import { DropdownUser } from '@components/UI/DropdownUser';
 
 const keaniaOne = Keania_One({
   weight: '400',
@@ -15,24 +15,32 @@ export const Header = () => {
   return (
     <header className="flex bg-white items-center justify-between px-10 py-3">
       <div className="flex items-center gap-14">
-        <p className={`${keaniaOne.className} text-4xl max-sm:hidden`}>
-          Trend Styles
-        </p>
-        <Image
-          className="hidden max-sm:block"
-          width={50}
-          height={50}
-          alt="Logo site"
-          src="/logo.svg"
-        />
+        <Link href="/">
+          <p className={`${keaniaOne.className} text-4xl max-sm:hidden`}>
+            Trend Styles
+          </p>
+          <Image
+            className="hidden max-sm:block"
+            width={50}
+            height={50}
+            alt="Logo site"
+            src="/logo.svg"
+          />
+        </Link>
         <div className="max-[914px]:hidden">
           <MenuLinks />
         </div>
       </div>
       <div className="flex gap-9 text-xl max-sm:gap-4">
-        <DropdownUser />
-        <FiHeart />
-        <FiShoppingCart />
+        <Link title="profile" href="/">
+          <FiUser />
+        </Link>
+        <Link title="my wishlist" href="/wishlist">
+          <FiHeart />
+        </Link>
+        <Link title="shopping cart" href="/">
+          <FiShoppingCart />
+        </Link>
         <div className="hidden max-[914px]:block">
           <MenuLinks />
         </div>
