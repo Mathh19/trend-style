b1c59e74b26074ec700036d096f9f63052bc689b
Matheus <mf635097@gmail.com>
2024-03-19 17:27:36 -0300

    fix: fix import

--- src/app/product/utils/checkQuantityStock.ts
+++ src/app/product/utils/checkQuantityStock.ts
@@ -1,4 +1,4 @@
-import { cartItemsProps } from '@shared-types/cart-items';
+import { CartItemsProps } from '@shared-types/cart-items';
 
 import products from '../../../contents/all-products.json';
 
@@ -9,7 +9,7 @@ export const checkQuantityStock = (productId: number, quantity: number) => {
 
   const cartItems = localStorage.getItem('cartItems');
   if (cartItems) {
-    const cartItemsArray: cartItemsProps[] = JSON.parse(cartItems);
+    const cartItemsArray: CartItemsProps[] = JSON.parse(cartItems);
     const totalCartItems = cartItemsArray
       .filter((item) => item.id === productId)
       .reduce((acc, currentItem) => acc + currentItem.quantity, 0);
