ae5b7760df4eef8a4a40cf2428a303f722a0ff65
Matheus <mf635097@gmail.com>
2024-03-25 14:47:38 -0300

    feat: add dynamic product link

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { MdOutlineStar } from 'react-icons/md';
 import { TbShoppingCartPlus } from 'react-icons/tb';
 
@@ -9,6 +12,12 @@ import { Button } from './UI/Button';
 import { Price } from './UI/Price';
 
 export const ProductCard = ({ product }: { product: ProductProps }) => {
+  const pathname = usePathname();
+  const link =
+    pathname === `/product/${product.id}`
+      ? product.id.toString()
+      : `/product/${product.id}`;
+
   return (
     <div className="relative w-72 h-92 border drop-shadow-md">
       <div className="absolute flex w-full justify-between p-2">
@@ -20,9 +29,9 @@ export const ProductCard = ({ product }: { product: ProductProps }) => {
         />
       </div>
       <img src={product.img[0]} alt={product.name} className="w-full h-full" />
-      <Link href={`./product/${product.id}`} className="group">
+      <Link href={link} className="group">
         <div className="absolute bg-white w-full bottom-0 p-2 space-y-1">
-          <div className="flex justify-between">
+          <div className="flex flex-wrap justify-between">
             <p className="text-left group-hover:underline">{product.name}</p>
             <Price price={product.price} discount={product.discount} />
           </div>
