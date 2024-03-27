35c6ea7b393202e3adb100c9e1b1177996a121a3
Matheus <mf635097@gmail.com>
2024-03-27 17:55:12 -0300

    chore: add shadcnui lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  prefix: '',
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px'
+        }
+      },
       screens: {
         sm: '640px',
         md: '768px',
@@ -15,8 +24,8 @@ module.exports = {
       },
       keyframes: {
         dropdown: {
-          '0%': { opacity: 0, transform: 'translateY(-0.5rem)' },
-          '100%': { opacity: 1, transform: 'translateY(0.5rem)' }
+          '0%': { opacity: '0', transform: 'translateY(-0.5rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0.5rem)' }
         },
         shake: {
           '0%, 100%': { transform: 'translateX(0)' },
@@ -26,14 +35,24 @@ module.exports = {
         'fade-in-slowly': {
           '0%': { opacity: '0' },
           '100%': { opacity: '100%' }
+        },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
         }
       },
       animation: {
         dropdown: 'dropdown 400ms ease-in-out',
         shake: 'shake 0.4s',
-        'fade-in-slowly': 'fade-in-slowly 0.3s ease-in-out'
+        'fade-in-slowly': 'fade-in-slowly 0.3s ease-in-out',
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
-  plugins: []
+  plugins: [require('tailwindcss-animate')]
 };
